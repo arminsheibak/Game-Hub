4563c4d2557cacf78208e062272feeaf1e686a38
Armin <armin2003sheibak@gmail.com>
2024-07-21 09:08:57 +0330

    clean up game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({gameQuery} : Props) => {
   return (
     <>
       {error && <Text> {error} </Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={4} padding={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={5} padding={10}>
         {isLoading && skeletons.map(s => <GameCardSkeleton key={s} />)}
         {data.map((game) => (
           <GameCard key={game.id} game={game} />
